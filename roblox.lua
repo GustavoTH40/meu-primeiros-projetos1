f736b23706ef6b47b52846ab3f51ea461d9c0bb0
GustavoTH40 <96442299+GustavoTH40@users.noreply.github.com>
2024-07-10 18:09:51 -0300

    Update roblox.lua

--- roblox.lua
+++ roblox.lua
@@ -26,3 +26,59 @@ local KillAll = Divider:Button{
        print("All players killed.")
    end
 }
+
+local LoopKillAll = Divider:Toggle{
+   Name = "Loop kill all",
+   Description = "Loop kills everyone in the game.",
+   Callback = function(State)
+       print("Kill state: ", State)
+   end
+}
+
+local OtherToggleStyle = Divider:Toggle{
+   Name = "2nd style of toggle",
+   Style = 2
+}
+
+local Players = Divider:Dropdown{
+   Name = "Player list",
+   Options = {"Player1", "Player2", "Player3", "Player4", "Player5"},
+   Callback = function(Value)
+       print(Value)
+   end
+}
+
+Divider:ColorPicker{
+   Name = "ESP color",
+   Default = Color3.fromRGB(0, 255, 255), -- default,
+   Callback = function(Value)
+       print(Value)
+   end
+}
+
+Divider:Box{
+   Name = "Car name",
+   ClearText = true, -- whether the textbox clears on focus or not
+   Callback = function(Value)
+       print(Value)
+   end
+}
+
+Divider:SearchDropdown{
+   Name = "Teleports",
+   Options = {"Pleasant Park", "Loot Lake", "Tomato Town", "Wailing Woods", "Anarchy Acres", "Retail Row"},
+   ClearText = false, -- default
+   Callback = function(Value)
+       print(Value)
+   end
+}
+
+local Quit = QuitDivider:Button{
+   Name = "Closes the ui library.",
+   Callback = function()
+       UI:Quit{
+           Message = "Fuck off...", -- closing message
+           Length = 1 -- seconds the closing message shows for
+       }
+   end
+}
