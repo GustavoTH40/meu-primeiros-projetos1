7368115ce7a23c5848f89b0049d15aaea74cab71
GustavoTH40 <96442299+GustavoTH40@users.noreply.github.com>
2024-07-10 18:05:00 -0300

    Update roblox.lua

--- roblox.lua
+++ roblox.lua
@@ -7,15 +7,15 @@ local UI = Lib:Create{
 }
 
 local Main = UI:Tab{
-   Name = "Main"
+   Name = "Inicio"
 }
 
 local Divider = Main:Divider{
-   Name = "Main shit"
+   Name = "Inicio shit"
 }
 
 local QuitDivider = Main:Divider{
-   Name = "Quit"
+   Name = "Sair
 }
 
 -- All functions have the Name, Description and Callback arguments so you can use them whenever ig yeah
@@ -24,61 +24,5 @@ local KillAll = Divider:Button{
    Description = "Kills all the players in the game!",
    Callback = function()
        print("All players killed.")
-   end
-}
-
-local LoopKillAll = Divider:Toggle{
-   Name = "Loop kill all",
-   Description = "Loop kills everyone in the game.",
-   Callback = function(State)
-       print("Kill state: ", State)
-   end
-}
-
-local OtherToggleStyle = Divider:Toggle{
-   Name = "2nd style of toggle",
-   Style = 2
-}
-
-local Players = Divider:Dropdown{
-   Name = "Player list",
-   Options = {"Player1", "Player2", "Player3", "Player4", "Player5"},
-   Callback = function(Value)
-       print(Value)
-   end
-}
-
-Divider:ColorPicker{
-   Name = "ESP color",
-   Default = Color3.fromRGB(0, 255, 255), -- default,
-   Callback = function(Value)
-       print(Value)
-   end
-}
-
-Divider:Box{
-   Name = "Car name",
-   ClearText = true, -- whether the textbox clears on focus or not
-   Callback = function(Value)
-       print(Value)
-   end
-}
-
-Divider:SearchDropdown{
-   Name = "Teleports",
-   Options = {"Pleasant Park", "Loot Lake", "Tomato Town", "Wailing Woods", "Anarchy Acres", "Retail Row"},
-   ClearText = false, -- default
-   Callback = function(Value)
-       print(Value)
-   end
-}
-
-local Quit = QuitDivider:Button{
-   Name = "Closes the ui library.",
-   Callback = function()
-       UI:Quit{
-           Message = "Fuck off...", -- closing message
-           Length = 1 -- seconds the closing message shows for
-       }
    end
 }
